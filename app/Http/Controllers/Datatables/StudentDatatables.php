949da40f4997842c265fffbaed3a0ae3571e607d
IndraA3 <indra.ardiansyah@sempoa.id>
2023-04-10 16:20:18 +0700

    wip: Student Tuitions Master

--- app/Http/Controllers/Datatables/StudentDatatables.php
+++ app/Http/Controllers/Datatables/StudentDatatables.php
@@ -20,7 +20,14 @@ class StudentDatatables extends Controller
                             $data = [
                                 'edit_url'     => route('students.edit', ['student' => $row->id]),
                                 'delete_url'   => route('students.destroy', ['student' => $row->id]),
-                                'redirect_url' => route('students.index')
+                                'redirect_url' => route('students.index'),
+
+                                'custom_links' => [
+                                    [
+                                        'label' => 'Biaya Khusus',
+                                        'url' => route('students.tuition-master', ['student' => $row->id]),
+                                    ]
+                                ]
                             ];
                             return view('components.datatable-action', $data);
                         })->toJson();
