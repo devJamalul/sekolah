87178c56165606406d25c996b597f369f4b0b8b3
IndraA3 <indra.ardiansyah@sempoa.id>
2023-04-13 16:45:02 +0700

    fix: Student Tuitions Master Bugs

--- app/Http/Controllers/StudentTuitionMasterController.php
+++ app/Http/Controllers/StudentTuitionMasterController.php
@@ -17,7 +17,6 @@ class StudentTuitionMasterController extends Controller
     public function index(Request $req)
     {    
         $test = StudentTuitionMaster::with('tuition.tuition_type')->latest('created_at')->get();
-        // dd($test[0]->tuition->tuition_type->name);
 
         $student = Student::findOrFail($req->id);
         $data = [
