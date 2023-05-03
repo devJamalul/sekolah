53db47b9d5750972947d51efd10fa8527713441f
IndraA3 <indra.ardiansyah@sempoa.id>
2023-05-03 22:16:42 +0700

    feat: Student Report ヾ(≧ ▽ ≦)ゝ

--- app/Exports/StudentsExport.php
+++ app/Exports/StudentsExport.php
@@ -2,12 +2,11 @@
 
 namespace App\Exports;
 
-use App\Models\Classroom;
-use App\Models\Student;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class StudentsExport implements FromView
+class StudentsExport implements FromView, WithColumnWidths
 {
     function __construct(protected $request)
     {
@@ -15,36 +14,18 @@ class StudentsExport implements FromView
 
     public function view(): View
     {
-        $request = $this->request;
+        $students = $this->request;
+        return view('exports.report-students', compact('students'));
+    }
 
-        $query = Classroom::query()->with('students')->where('school_id', session('school_id'));
-
-        $query->when($request->academic_year, function($query) use($request){
-            $query->where('academic_year_id', $request->academic_year);
-        });
-
-        $query->when($request->grade, function($query) use($request){
-            $query->where('grade_id', $request->grade);
-        });
-
-        $query->when($request->classroom, function($query) use($request){
-            $query->where('id', $request->classroom);
-        });
-
-        $classrooms = $query->get();
-
-        $students = collect($classrooms)->map(function($classroom){
-            if ($classroom->students) {
-                return collect($classroom)->map(function($student){
-                    return $student;
-                });
-            }
-        })->pluck('students')->collapse();
-
-        
-        dump($classrooms);
-        dd($students);
-
-        return view('exports.report-students-excel', compact('students'));
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 10,
+            'B' => 40,            
+            'C' => 20,            
+            'D' => 40,            
+            'E' => 20,            
+        ];
     }
 }
--- app/Http/Controllers/Reports/StudentReport.php
+++ app/Http/Controllers/Reports/StudentReport.php
@@ -8,7 +8,9 @@ use App\Models\AcademicYear;
 use App\Models\Classroom;
 use App\Models\Grade;
 use App\Models\Student;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Excel;
 
 class StudentReport extends Controller
@@ -46,12 +48,47 @@ class StudentReport extends Controller
 
     public function exportStudentReport(Request $request)
     {
+        $query = null;
+
+        // Show Filtered By Academic Year / Grade if param exists
+        if ($request->academic_year || $request->grade) {
+            $query = Classroom::query()->with('students.classrooms.grade')->where('school_id', session('school_id'));
+
+            // Filter By Academic Year (If Exist)
+            $query->when($request->academic_year, function($query) use($request){
+                $query->where('academic_year_id', $request->academic_year);
+            });
+
+            // Filter By Grade (If Exist)
+            $query->when($request->grade, function($query) use($request){
+                $query->where('grade_id', $request->grade);
+            });
+
+            // Filter By Classroom (If Exist)
+            $query->when($request->classroom, function($query) use($request){
+                $query->where('id', $request->classroom);
+            });
+            
+            $classrooms = $query->get();
+
+            $students = collect($classrooms)->map(function($classroom){
+                if ($classroom->students) {
+                    return collect($classroom)->map(function($student){
+                        return $student;
+                    });
+                }
+            })->pluck('students')->collapse();
+
+        } else {
+            $students = Student::where('school_id', session('school_id'))->get();
+        }
+
         switch ($request->action) {
             case 'excel':
-                return Excel::download(new StudentsExport($request), 'students.xlsx');
+                return Excel::download(new StudentsExport($students), "data_murid_". Carbon::parse(now())->format('d-m-Y') .".xlsx");
                 break;
             case 'pdf':
-                dd("Export to PDF");
+                return PDF::loadView('exports.report-students', compact('students'))->download("data_murid_". Carbon::parse(now())->format('d-m-Y') .".pdf");
                 break;
         }
     }
