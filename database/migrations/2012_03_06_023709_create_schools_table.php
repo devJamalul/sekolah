b6f4036d4b5094dd7c77bab4e90d345e08158c09
AaEzha <reza.nurfachmi@sempoa.id>
2023-05-15 10:25:03 +0700

    squash: schools

--- database/migrations/2012_03_06_023709_create_schools_table.php
+++ database/migrations/2012_03_06_023709_create_schools_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\School;
+use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +16,18 @@ return new class extends Migration
     {
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->enum('grade', ["TK", "SD", "SMP", "SMA", "SMK"])->nullable();
+            $table->text('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('postal_code', 20)->nullable();
+            $table->string('school_name');
+            $table->string('foundation_head_name')->nullable();
+            $table->string('foundation_head_tlpn')->nullable();
+            $table->foreignIdFor(Staff::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(School::class)->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
