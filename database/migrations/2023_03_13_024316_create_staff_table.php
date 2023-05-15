1c95c270a13d539be62efca831cd4efd351b0432
AaEzha <reza.nurfachmi@sempoa.id>
2023-05-15 10:57:26 +0700

    squash: staff

--- database/migrations/2023_03_13_024316_create_staff_table.php
+++ database/migrations/2023_03_13_024316_create_staff_table.php
@@ -18,6 +18,18 @@ return new class extends Migration
             $table->foreignIdFor(School::class)->nullable()->constrained()->nullOnDelete();
             $table->foreignIdFor(User::class)->nullable()->constrained()->nullOnDelete();
             $table->string('name')->nullable();
+            $table->string('gender', 10)->nullable();
+            $table->text('address')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('religion', 10)->nullable();
+            $table->string('phone_number', 15)->nullable();
+            $table->string('nik', 16)->nullable()->comment('Nomor induk kependudukan');
+            $table->string('nidn', 20)->nullable()->comment('Nomor induk dosen nasional');
+            $table->string('nip', 20)->nullable()->format('Nomor induk pegawai');
+            $table->string('family_card_number', 25)->nullable()->comment('Nomor kartu keluarga');
+            $table->string('file_photo')->nullable();
+            $table->string('file_birth_certificate')->nullable();
+            $table->string('file_family_card')->nullable()->comment('Kartu keluarga');
             $table->timestamps();
             $table->softDeletes();
         });
