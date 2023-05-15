9b0d97e57688de523aa6a7faa10d9e2d75767efb
AaEzha <reza.nurfachmi@sempoa.id>
2023-05-15 17:01:29 +0700

    fix: Informasi sekolah 🔥

--- resources/views/pages/school/profile/index.blade.php
+++ resources/views/pages/school/profile/index.blade.php
@@ -168,7 +168,7 @@
                                 @enderror
                             </div>
 
-                            @can('profile-update.update')
+                            @can('schools.profile-index')
                             <div class="btn-group float-right mr-3 mt-2">
                                 <button type="submit" class="btn btn-primary ">Simpan</button>
                                 <button type="reset" class="btn btn-secondary ">Batal</button>
