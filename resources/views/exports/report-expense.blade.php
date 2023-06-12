0f60dde14ff0678c90b137a39dcb5c589618d7bb
reynold <reynold@marketport.io>
2023-06-12 15:20:22 +0700

    fix report expense

--- resources/views/exports/report-expense.blade.php
+++ resources/views/exports/report-expense.blade.php
@@ -6,7 +6,10 @@
             <th width="30%"><strong>Tanggal Pengeluaran Biaya</strong></th>
             <th width="30%"><strong>Total Biaya</strong></th>
             <th width="30%"><strong>Tanggal Realisasi</strong></th>
-            <th width="30%"><strong>Bukti</strong></th>
+            {{-- @dd($expense)
+            @if ($expense->file_photo) --}}
+                <th width="30%"><strong>Bukti</strong></th>
+            {{-- @endif --}}
         </tr>
     </thead>
     <tbody>
@@ -16,8 +19,13 @@
                 <td>{{ $row['expense_number'] }}</td>
                 <td>{{ $row['expense_date'] }}</td>
                 <td>Rp. {{ number_format($row->expense_details()->sum(DB::raw('price * quantity')), 0, ', ', '.') }}</td>
+                @if ($row['status'] == 'rejected')
+                <td>-</td>
+                <td>-</td>
+                @else
                 <td>{{ $row['expense_outgoing_date'] }}</td>
-                <td><a href="{{ $row['file_photo'] }}">Download</a></td>
+                <td><a href="{{ $row['file_photo'] }}" target="_blank" download>Download</a></td>
+                @endif
             </tr>
         @endforeach
     </tbody>
