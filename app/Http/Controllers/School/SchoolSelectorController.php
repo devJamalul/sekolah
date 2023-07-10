f46f083878867db28859fe399ffa8132be0e35cc
AaEzha <reza.nurfachmi@sempoa.id>
2023-07-10 11:40:38 +0700

    fix: school selector is null 🦊

--- app/Http/Controllers/School/SchoolSelectorController.php
+++ app/Http/Controllers/School/SchoolSelectorController.php
@@ -14,31 +14,43 @@ class SchoolSelectorController extends Controller
      */
     public function __invoke(Request $request): void
     {
-        $school = School::findOrFail($request->school_selector);
-        $this->save($school);
+        try {
+            $school = School::findOrFail($request->school_selector);
+            if (!$school) {
+                throw new \Exception('No school selected');
+            }
+            $this->save($school);
+        } catch (\Throwable $th) {
+            session()->forget(['academic_year_id', 'academic_year_name', 'ppdb_academic_year_id', 'ppdb_academic_year_name']);
+        }
     }
 
     protected function save(School $school): void
     {
-        $key = "school_id";
-        session([$key => $school->getKey()]);
+        try {
+            info($school);
+            $key = "school_id";
+            session([$key => $school->getKey()]);
 
-        $academicYear = AcademicYear::active()->first();
+            $academicYear = AcademicYear::active()->first();
 
-        if (!is_null($academicYear)) {
-            session(['academic_year_id' => $academicYear->id]);
-            session(['academic_year_name' => $academicYear->academic_year_name]);
-        } else {
-            session()->forget(['academic_year_id', 'academic_year_name']);
-        }
+            if (!is_null($academicYear)) {
+                session(['academic_year_id' => $academicYear->id]);
+                session(['academic_year_name' => $academicYear->academic_year_name]);
+            } else {
+                session()->forget(['academic_year_id', 'academic_year_name']);
+            }
 
-        $ppdb = AcademicYear::PPDB()->first();
+            $ppdb = AcademicYear::PPDB()->first();
 
-        if (!is_null($ppdb)) {
-            session(['ppdb_academic_year_id' => $ppdb->id]);
-            session(['ppdb_academic_year_name' => $ppdb->academic_year_name]);
-        } else {
-            session()->forget(['ppdb_academic_year_id', 'ppdb_academic_year_name']);
+            if (!is_null($ppdb)) {
+                session(['ppdb_academic_year_id' => $ppdb->id]);
+                session(['ppdb_academic_year_name' => $ppdb->academic_year_name]);
+            } else {
+                session()->forget(['ppdb_academic_year_id', 'ppdb_academic_year_name']);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
     }
 }
