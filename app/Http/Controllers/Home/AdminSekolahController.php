8a4cb19bd827daeac7287a4af99549210e9b7e5f
Reynold <reynold@pop-os.localdomain>
2023-07-10 09:01:45 +0700

    added staff & students to dashboard

--- app/Http/Controllers/Home/AdminSekolahController.php
+++ app/Http/Controllers/Home/AdminSekolahController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Models\Grade;
 use App\Http\Controllers\Controller;
+use App\Models\Classroom;
+use App\Models\ClassroomStudent;
+use App\Models\Staff;
 
 class AdminSekolahController extends Controller
 {
@@ -11,6 +15,20 @@ class AdminSekolahController extends Controller
      */
     public function __invoke()
     {
-        return view('pages.home.admin-sekolah');
+
+        $data['total_students'] = 'Total Students';
+        $data['total_students_class'] = 'dark';
+        $data['grades'] = Grade::get();
+        $data['classrooms'] = Classroom::with('academic_year')->get();
+        $data['classroomStudent'] = ClassroomStudent::get();
+
+
+        $data['total_staff'] = 'Total Staff';
+        $data['total_staff_class'] = 'dark';
+        $data['staff'] = Staff::get()->count();
+        $data['staff_icon'] = 'fas fa-users';
+
+
+        return view('pages.home.admin-sekolah', $data);
     }
 }
--- app/Http/Controllers/Home/TataUsahaController.php
+++ app/Http/Controllers/Home/TataUsahaController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Models\Grade;
+use App\Models\Staff;
+use App\Models\Classroom;
+use App\Models\ClassroomStudent;
 use App\Http\Controllers\Controller;
 
 class TataUsahaController extends Controller
@@ -11,6 +15,18 @@ class TataUsahaController extends Controller
      */
     public function __invoke()
     {
-        return view('pages.home.tata-usaha');
+        $data['total_students'] = 'Total Students';
+        $data['total_students_class'] = 'dark';
+        $data['grades'] = Grade::get();
+        $data['classrooms'] = Classroom::with('academic_year')->get();
+        $data['classroomStudent'] = ClassroomStudent::get();
+
+
+        $data['total_staff'] = 'Total Staff';
+        $data['total_staff_class'] = 'dark';
+        $data['staff'] = Staff::get()->count();
+        $data['staff_icon'] = 'fas fa-users';
+
+        return view('pages.home.tata-usaha', $data);
     }
 }
